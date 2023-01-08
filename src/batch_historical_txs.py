d021abc582d6f360d80d4cc4c66dd1d8c753873c
marcoaureliomenezes <marcoaurelioreislima@gmail.com>
2023-01-08 10:10:29 -0300

    Batch_historical_txs app and tx_input_converters app ready to use. Needed to fix: Smart contracts that use some proxy create a missmatch between ABI code got and the input contract address. Because of this Aave inputs cannot be decoded.

--- src/batch_historical_txs.py
+++ src/batch_historical_txs.py
@@ -1,23 +1,14 @@
 from functools import reduce
 import time, os, sys, logging
-import pandas as pd
-import web3.eth as eth
 from datetime import datetime
 from utils.etherscan_api import get_txlist_url, get_block_by_time_url, req_chain_scan
 from utils.utils import get_kafka_producer
 
-pd.options.display.max_colwidth = 100
 logging.basicConfig(level='INFO')
 
-def get_block_interval(api_key, start_date, end_date=None):
-    print(api_key, "API KEY")
-    """METHOD get_block_interval: Receives 2 datetime objects representing a time interval
-    PARM 1: API Key to use the network scan.
-    PARM 2: Name of the network used.
-    PARM 3: Address of the smart contract.
-    PARM 4: Interval of blocks to get the transactions.
-    RETURN a list of transactions interacting with the 
-    """
+
+def get_block_interval(start_date, end_date=None): 
+    api_key = os.environ['SCAN_API_KEY']
     start_date, end_date = [int(datetime.timestamp(i)) for i in [start_date, end_date]]
     block_bottom = req_chain_scan(api_key, get_block_by_time_url, dict(timestamp=start_date, closest='after'))
     block_top = req_chain_scan(api_key, get_block_by_time_url, dict(timestamp=end_date, closest='before'))
@@ -25,25 +16,14 @@ def get_block_interval(api_key, start_date, end_date=None):
 
 
 def produce_smart_contract_txs(producer, transaction):
-    """METHOD produce_smart_contract_txs: Send a transaction to a Kafka Topic.
-    PARM 1: transaction represented as a dictionary..
-    PARM 2: A Kafka Producer object instanciated.
-    PARM 3: The name  of the topic, in order to send the transaction message.
-    RETURN a list of transactions with the fields of interest and rename these fields
-    """
     topic = os.environ['TOPIC_BATCH_TX']
     try: producer.send(topic=topic, value=transaction)
     except: logging.error(f'Error sending message through topic {topic}') ; return False
     else: return True  
 
 
-def get_transactions(api_key, address, startblock, endblock):
-    """METHOD get_transactions:
-    PARM 2: API Key to use with network scan.
-    PARM 4: Block since when the module must download contract's transactions.
-    PARM 5: Block till when the module must download contract's transactions.
-    RETURN a list of transactions.
-    """
+def get_transactions(address, startblock, endblock):
+    api_key = os.environ['SCAN_API_KEY']
     parms_get_txlist_url = dict(
                 address=address, 
                 startblock=startblock, 
@@ -54,12 +34,8 @@ def get_transactions(api_key, address, startblock, endblock):
 
 
 def handle_smart_contract_txs(transactions):
-    """METHOD handle_smart_contract_txs: retrieve transactions cleaned with the interest fields handled
-    PARM 1: List of transactions. A list of dictionaries where each dictionary represents a transaction.
-    RETURN a list of transactions with the fields of interest renamed.
-    """
-    interest_cols = ['blockNumber','timeStamp','from', 'to', 'isError','value','nonce','gasPrice','gasUsed','functionName', 'input']
-    renamed_cols = ['block_number','timestamp','from','to','is_error','value','nonce','gasPrice','gasUsed','method', 'input']
+    interest_cols = ['hash', 'blockNumber','timeStamp','from', 'to', 'isError','value','nonce','gasPrice','gasUsed','functionName', 'input']
+    renamed_cols = ['tx_id','block_number','timestamp','from','to','is_error','value','nonce','gasPrice','gasUsed','method', 'input']
     tx_filtered = [{col: transaction[col] for col in interest_cols} for transaction in transactions]
     tx_renamed = [{renamed_cols[col]: tx[interest_cols[col]] for col in range(len(renamed_cols))} for tx in tx_filtered]
     my_split = lambda x: x.split("(")[0]
@@ -68,53 +44,33 @@ def handle_smart_contract_txs(transactions):
     return tx_renamed
 
 
-def batch_contract_txs(producer, api_key, block_bottom, block_top):
-
-    """METHOD batch_contract_txs: Downloads address transactions in batch and send it trhough a topic 
-    in kafka using recursion to complete the JOB.
-    PARM 1: A Kafka Producer object to use when producing data.
-    PARM 2: Block since when the module must download contract's transactions.
-    PARM 3: Block till when the module must download contract's transactions.
-    This method is called till we reach the state whe less then 100 blocks are missing.
-    """
-    print(api_key, os.environ['ADDRESS_TO_SCAN'], block_bottom, block_top)
-    list_gross_tx = get_transactions(api_key, os.environ['ADDRESS_TO_SCAN'], block_bottom, block_top)
+def batch_contract_txs(producer, contract_address, block_bottom, block_top):
+    list_gross_tx = get_transactions(contract_address, block_bottom, block_top)
+    if list_gross_tx == False: return "COMPLETED"
     list_cleaned_tx = handle_smart_contract_txs(list_gross_tx)
-    time.sleep(1)
-    print(list_cleaned_tx)
     for tx in list_cleaned_tx:
         time.sleep(0.00001)
         produce_smart_contract_txs(producer, tx)
-
     next_bottom = max([int(i['block_number']) for i in list_cleaned_tx])
-    print(block_top, next_bottom)
-    print(len(list_cleaned_tx))
     if (block_top - next_bottom > 100) and (len(list_cleaned_tx) > 1):
         logging.info(f"Blocks to be analysed: {block_top - next_bottom}")
-        batch_contract_txs(producer, api_key, next_bottom, block_top)
+        batch_contract_txs(producer, contract_address, next_bottom, block_top)
     else:
         return "COMPLETED"
 
 
 if __name__ == '__main__':
-    """
-    MODULE: batch_historical.py
-    KEY VALUE PARAMETERS:
-        start_date: Date since when the module must download contract's transactions. 
-        end_date: Date till when the module must download contracts's transactions.
-    Get transactions made by a smart contract in a specific blockchain network.
-    """
-    api_keys = [os.environ[i] for i in os.environ if i[:12] == 'SCAN_API_KEY']
     parms = {k: v for k, v in [i.split("=") for i in sys.argv[1:]]}
     try:
         start_date = parms['start_date']
+        contract_address = parms['contract_address']
     except KeyError as e:
-        logging.error(f"Parameter start_date='some date' (in format %Y-%m-%d) are required")
+        logging.error(f"Parameters start_date and contract_address are required.")
     else:
         end_date = parms.get('end_date')
-        producer = get_kafka_producer()
         start_date = datetime.strptime(start_date, '%Y-%m-%d')
         end_date = datetime.strptime(end_date, '%Y-%m-%d') if end_date else datetime.now()
-        block_bottom, block_top =  get_block_interval(api_keys[0], start_date, end_date=end_date)
-        res = batch_contract_txs(producer, api_keys[0], block_bottom, block_top)
+        block_bottom, block_top =  get_block_interval(start_date, end_date=end_date)
+        producer = get_kafka_producer()
+        res = batch_contract_txs(producer, contract_address, block_bottom, block_top)
 
--- src/tx_input_converters.py
+++ src/tx_input_converters.py
@@ -1,21 +1,10 @@
-import json, requests, os
-from web3 import Web3
-import web3
-from utils.utils import setup_database, get_kafka_producer, get_kafka_consumer
-from utils.etherscan_api import req_chain_scan, get_abi_url
-import traceback
-import sys
+import json, sys, logging, os
 from functools import lru_cache
 from web3 import Web3
-from web3.auto import w3
-from web3.contract import Contract
-from web3._utils.events import get_event_data
-from web3._utils.abi import exclude_indexed_event_inputs, get_abi_input_names, get_indexed_event_inputs, normalize_event_input_types
-from web3.exceptions import MismatchedABI, LogTopicError
-from web3.types import ABIEvent
-from eth_utils import event_abi_to_log_topic, to_hex
-from hexbytes import HexBytes
+from utils.utils import get_kafka_producer, get_kafka_consumer
+from utils.etherscan_api import req_chain_scan, get_abi_url 
 
+logging.basicConfig(level='INFO')
 
 @lru_cache(maxsize=None)
 def _get_contract(address, abi):
@@ -29,21 +18,36 @@ def _get_contract(address, abi):
   contract = web3.eth.contract(address=Web3.toChecksumAddress(address), abi=abi)
   return (contract, abi)
 
+
+def convert_input_transactions(producer, consumer, contract):
+  for msg in consumer:
+    detail_transaction = json.loads(msg.value)
+    try: _, parms_method = contract.decode_function_input(detail_transaction['input'])
+    except: parms_method = detail_transaction['input']
+    payload = dict(tx_id=detail_transaction['tx_id'], method=detail_transaction['method'], parms=parms_method)
+    try:
+      producer.send(topic=topic_output, value=payload)
+    except TypeError: pass
+
+
 if __name__ == '__main__':
+    api_key_node = os.environ['NODE_API_KEY']
+    api_key_scan = os.environ['SCAN_API_KEY']
+    network = os.environ["NETWORK"]
+    topic_input = os.environ['TOPIC_INPUT']
+    topic_output = os.environ['TOPIC_OUTPUT']
+    consumer_group = os.environ['CONSUMER_GROUP']
 
-    list_api_key = [os.environ[i] for i in os.environ if i[:12] == 'NODE_API_KEY']
-    counter_key = 0
-    key_index = list_api_key[counter_key % len(list_api_key)]
-    web3 = Web3(Web3.HTTPProvider(f'https://{os.environ["NETWORK"]}.infura.io/v3/{key_index}'))
+    parms = {k: v for k, v in [i.split("=") for i in sys.argv[1:]]}
+    try: 
+      contract_address = parms['contract_address']
+      auto_offset_reset = parms['auto_offset_reset']
+    except KeyError as e: logging.error(f"Parameters contract_address and auto_offset_reset are required.") 
 
-    consumer = get_kafka_consumer(os.environ['TOPIC_INPUT'], os.environ['CONSUMER_GROUP'])
+    web3 = Web3(Web3.HTTPProvider(f'https://{network}.infura.io/v3/{api_key_node}'))
+    consumer = get_kafka_consumer(topic_input, consumer_group, auto_offset_reset=auto_offset_reset)
     producer = get_kafka_producer()
-    api_key = os.environ['SCAN_API_KEY']
-for msg in consumer:
-        detail_transaction = json.loads(msg.value)
-        abi = req_chain_scan(api_key, get_abi_url, dict(address=detail_transaction['to']))
-        contract, abi = _get_contract(address=detail_transaction['to'], abi=abi)
-        func_obj, func_params = contract.decode_function_input(detail_transaction['input'])
-        detail_transaction['func_obj'] = str(func_obj)
-        detail_transaction['input'] = func_params
-        producer.send(topic=os.environ['TOPIC_OUTPUT'], value=detail_transaction)
+    abi = req_chain_scan(api_key_scan, get_abi_url, dict(address=contract_address))
+    contract, _ = _get_contract(address=contract_address, abi=abi)
+    convert_input_transactions(producer, consumer, contract)
+
